a730258a0de4a0ea0361495b1102bf5e86d810e9
Yamaf <upripadcheva@gmail.com>
2026-09-15 11:40:25 +0300

    сообщение

---
+++ src/lab1/ex3.py
@@ -0,0 +1,9 @@
+price = float(input())
+discount = int(input())
+vat = int(input())
+base = price * (1 - discount/100)
+vat_amount = base * (vat/100)
+total = base + vat_amount
+print(f"База после скидки: {base:.2f} ₽")
+print(f"НДС: {vat_amount:.2f} ₽")
+print(f"Итого к оплате: {total:.2f} ₽")
